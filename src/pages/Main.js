069e6c53cd36e36d21261b437afc172bf09a1475
Reydv <reyhanputraariutama@gmail.com>
2025-03-16 22:12:36 +0700

    Update visual Main.js

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -2,25 +2,41 @@ import Footer from "../components/Footer";
 
 function Main() {
   return (
-    <div className="min-h-screen flex flex-col items-center">
+    <div className="min-h-screen flex flex-col items-center relative overflow-hidden">
+      <div
+        className="absolute inset-0 w-[200%] h-[200%] top-[-26%]"
+        style={{
+          background:
+            "linear-gradient(15deg, transparent 40%, rgba(119, 105, 83, 0.15) 50%, transparent 55%)",
+        }}
+      ></div>
+
       {/* Main Content */}
-      <main className="flex-1 flex flex-col items-center justify-center text-center px-4">
-        <h1 className="text-accent text-4xl md:text-5xl font-bold mb-2 uppercase tracking-wider">
-          Sic Semper Tyrannis
+      <main className="flex-1 flex flex-col items-center justify-center text-center z-[1] px-4">
+        {/* Heading */}
+        <h1 className="font-serif leading-[1.1] text-accent text-[40px] sm:text-[50px] md:text-[64px] font-bold mb-[20px] uppercase tracking-[-0.03em]">
+          <span>Sic Semper</span> <br />
+          <span>Tyrannis</span>
         </h1>
-        <h4 className="text-white text-lg mb-8">Memento mori</h4>
-        <div className="space-y-4 flex flex-col">
+
+        {/* Subheading */}
+        <h4 className="text-white text-[24px] sm:text-[30px] mb-[40px] sm:mb-[65px] font-[Merryweather] tracking-[0.15em]">
+          Memento mori
+        </h4>
+
+        {/* Buttons */}
+        <div className="space-y-[15px] flex flex-col w-full max-w-[400px]">
           <a
-            className="bg-accent text-white px-8 py-2 w-48 text-sm uppercase tracking-wider"
+            className="bg-accent text-white w-full sm:w-[400px] h-[48px] text-[16px] sm:text-[18px] font-serif font-bold tracking-[0.16em] uppercase hover:bg-[#FFFFFF] hover:text-[#080808] hover:border-[#FFFFFF] transition-all duration-300 flex justify-center items-center"
             href="/posts"
           >
             Konten Utama
           </a>
           <a
-            className="border-secondary border text-white px-8 py-2 w-48 text-sm uppercase tracking-wider"
-            href="/comunity"
+            className="border-[#3A3A3A] border text-secondary w-full sm:w-[400px] h-[48px] text-[16px] sm:text-[18px] font-serif font-bold tracking-[0.16em] uppercase hover:bg-[#3A3A3A] hover:text-[#FFFFFF] hover:border-[#D0D0D0] transition-all duration-300 flex justify-center items-center"
+            href="/posts"
           >
-            Komunitas
+            Konten Komunitas
           </a>
         </div>
       </main>
